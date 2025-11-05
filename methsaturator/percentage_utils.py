3ae7eed81786779695175763cf08463aae5e90dc
edogiuili <edoardogiuili@gmail.com>
2025-11-06 00:32:41 +0100

    removed 1 from if and add it to percentage

--- methsaturator/percentage_utils.py
+++ methsaturator/percentage_utils.py
@@ -7,9 +7,15 @@ def percentage_checker(percentages):
     """
     list_percentages = [float(x.strip()) for x in percentages.split(",")]
 
-    if any(p <= 0 or p >= 1 for p in list_percentages):
-        raise click.UserError("Percentages must be between 0 and 1 (exclusive).")
+    if any(p == 0 for p in list_percentages):
+        raise click.UserError("Percentages must be between > 0.")
 
     if len(list_percentages) < 4:
         raise click.UserError("At least four percentages must be provided.")
+
+    # And now add 1 if not persent to the list
+    # to calculate CpG number on original sample
+    if 1 not in list_percentages:
+        list_percentages = list_percentages + [1]
+
     return list_percentages
