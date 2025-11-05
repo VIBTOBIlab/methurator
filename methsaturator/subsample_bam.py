fba77c68af00dc00afeb3a78feed865671f89073
edogiuili <edoardogiuili@gmail.com>
2025-11-06 00:33:18 +0100

    added condition if percentage = 1

--- methsaturator/subsample_bam.py
+++ methsaturator/subsample_bam.py
@@ -10,25 +10,31 @@ def subsample_bam(bam_path, percentage, output_dir, seed=42):
     os.makedirs(bam_dir, exist_ok=True)
 
     round_pct = round(percentage, 2)
-    base_name = os.path.basename(bam_path).replace(
-        ".bam", f"_subsample_{round_pct:.1f}.bam"
-    )
-    output_path = os.path.join(bam_dir, base_name)
+    output_path = bam_path
 
-    # Subsample BAM using samtools
-    cmd = [
-        "samtools",
-        "view",
-        "-s",
-        str(round_pct),
-        "--subsample-seed",
-        str(seed),
-        "-b",
-        bam_path,
-        "-o",
-        output_path,
-    ]
-    subprocess.run(cmd, check=True)
+    # If percentage is different than 1, then subsample
+    if percentage != 1:
+
+        # Extract the sample name and create the output path
+        base_name = os.path.basename(bam_path).replace(
+            ".bam", f"_subsample_{round_pct:.1f}.bam"
+        )
+        output_path = os.path.join(bam_dir, base_name)
+
+        # Subsample BAM using samtools
+        cmd = [
+            "samtools",
+            "view",
+            "-s",
+            str(round_pct),
+            "--subsample-seed",
+            str(seed),
+            "-b",
+            bam_path,
+            "-o",
+            output_path,
+        ]
+        subprocess.run(cmd, check=True)
 
     # Index the subsampled BAM
     cmd2 = ["samtools", "index", output_path]
