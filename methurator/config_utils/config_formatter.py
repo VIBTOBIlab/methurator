5640e8eea92545f6b7560ea1e33304455c299803
edogiuili <edoardogiuili@gmail.com>
2025-11-30 17:36:04 +0100

    fixed typo in mm39

--- methurator/config_utils/config_formatter.py
+++ methurator/config_utils/config_formatter.py
@@ -4,7 +4,7 @@ GENOME_URLS = {
     "GRCh38": "http://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz",
     "GRCh37": "http://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh37.dna.primary_assembly.fa.gz",
     "mm10": "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.fa.gz",
-    "mm39": "https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm39.fa.gz",
+    "mm39": "https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.fa.gz",
 }
 
 
