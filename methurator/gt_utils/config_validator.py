ce3385815163733573eb974c518b2203e939e44d
edogiuili <edoardogiuili@gmail.com>
2026-01-22 12:01:22 +0100

    update config obj

--- methurator/gt_utils/config_validator.py
+++ methurator/gt_utils/config_validator.py
@@ -1,7 +1,8 @@
 class GTConfig:
 
-    def __init__(self, covs, **kwargs):
-        self.covs = covs
+    def __init__(self, bams, **kwargs):
+        self.bam = bams
+        self.covs = []
         self.outdir = kwargs.get("outdir")
         self.minimum_coverage = kwargs.get("minimum_coverage")
         self.t_step = kwargs.get("t_step")
@@ -12,4 +13,9 @@ class GTConfig:
         self.compute_ci = kwargs.get("compute_ci")
         self.bootstrap_replicates = kwargs.get("bootstrap_replicates")
         self.conf = kwargs.get("conf")
+        self.fasta = kwargs.get("fasta")
+        self.genome = kwargs.get("genome")
+        self.rrbs = kwargs.get("rrbs")
+        self.threads = kwargs.get("threads")
+        self.keep_temporary_files = kwargs.get("keep_temporary_files")
         self.verbose = kwargs.get("verbose")
